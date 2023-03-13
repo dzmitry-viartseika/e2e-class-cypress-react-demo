ca1361eb8a7205062cfb03e5a8ec9202b2b2808e
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-03-13 23:32:34 +0100

    implemented contact page test

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -3,13 +3,17 @@ const WEBSITE_LINK_ABOUT = 'http://localhost:5173/about';
 describe('contact page', () => {
   it('should submit the form', () => {
     const BUTTON_LOADING_TEXT = 'Sending...';
+    const BUTTON_DEFAULT_TEXT = 'Send Message';
 
     cy.visit(WEBSITE_LINK_ABOUT);
     cy.get('[data-cy="contact-input-message"]').type('Hello everybody');
     cy.get('[data-cy="contact-input-name"]').type('Dzmitry');
     cy.get('[data-cy="contact-input-email"]').type('test@gmail.com');
+    cy.get('[data-cy="contact-btn-submit"]').contains(BUTTON_DEFAULT_TEXT);
+    cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled')
     cy.get('[data-cy="contact-btn-submit"]').click();
 
     cy.get('[data-cy="contact-btn-submit"]').contains(BUTTON_LOADING_TEXT);
+    cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled')
   })
 })
